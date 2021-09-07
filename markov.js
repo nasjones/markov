722a66e6c1d10e82fe02192e4f0ab9d1f7ff5da1
nasjones <46979749+nasjones@users.noreply.github.com>
2021-09-07 18:56:50 -0400

    work completed

--- markov.js
+++ markov.js
@@ -1,29 +1,57 @@
 /** Textual markov chain generator */
-
-
 class MarkovMachine {
+	/** build markov machine; read in text.*/
 
-  /** build markov machine; read in text.*/
+	constructor(text) {
+		let words = text.split(/[ \r\n]+/);
+		this.words = words.filter((c) => c !== "");
+		this.makeChains();
+	}
 
-  constructor(text) {
-    let words = text.split(/[ \r\n]+/);
-    this.words = words.filter(c => c !== "");
-    this.makeChains();
-  }
+	/** set markov chains:
+	 *
+	 *  for text of "the cat in the hat", chains will be
+	 *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
-  /** set markov chains:
-   *
-   *  for text of "the cat in the hat", chains will be
-   *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
+	makeChains() {
+		// TODO
+		this.chains = {};
+		for (let i = 0; i < this.words.length - 1; i++) {
+			let current = this.words[i];
+			let next = this.words[i + 1];
+			let word = this.words[i + 2] ? this.words[i + 2] : null;
+			if (this.chains[`${current} ${next}`]) {
+				this.chains[`${current} ${next}`].push(word);
+			} else {
+				this.chains[`${current} ${next}`] = [word];
+			}
+		}
+	}
 
-  makeChains() {
-    // TODO
-  }
+	/** return random text from chains */
 
-
-  /** return random text from chains */
-
-  makeText(numWords = 100) {
-    // TODO
-  }
+	makeText(numWords = 100) {
+		// TODO
+		let output = "";
+		let chains = Object.keys(this.chains);
+		let current = chains[Math.floor(Math.random() * chains.length)];
+		while (output.split(" ").length < numWords) {
+			output += `${current}`;
+			let follows = this.chains[current];
+			let start = follows[Math.floor(Math.random() * follows.length)];
+			if (start == null) {
+				output += ".";
+				current = chains[Math.floor(Math.random() * chains.length)];
+			} else {
+				let search = chains.filter((key) =>
+					key.startsWith(`${start} `)
+				);
+				current = search[Math.floor(Math.random() * search.length)];
+			}
+			output += " ";
+		}
+		return output;
+	}
 }
+
+module.exports = { MarkovMachine };
